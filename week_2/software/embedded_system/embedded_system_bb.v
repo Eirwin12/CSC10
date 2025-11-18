309f4647d16cc4577eee52e4c33d0d2478e49702
Fickel <maakeirwin@gmail.com>
2025-11-18 21:11:10 +0100

    hex to 7 seg in de platform designer proberen stoppen

--- week_2/software/embedded_system/embedded_system_bb.v
+++ week_2/software/embedded_system/embedded_system_bb.v
@@ -2,9 +2,11 @@
 module embedded_system (
 	clk_clk,
 	reset_reset_n,
+	to_7seg_readdata,
 	to_hex_readdata);	
 
 	input		clk_clk;
 	input		reset_reset_n;
+	output	[6:0]	to_7seg_readdata;
 	output	[31:0]	to_hex_readdata;
 endmodule
--- week_2/software/embedded_system/embedded_system_inst.vhd
+++ week_2/software/embedded_system/embedded_system_inst.vhd
@@ -1,15 +1,17 @@
 	component embedded_system is
 		port (
-			clk_clk         : in  std_logic                     := 'X'; -- clk
-			reset_reset_n   : in  std_logic                     := 'X'; -- reset_n
-			to_hex_readdata : out std_logic_vector(31 downto 0)         -- readdata
+			clk_clk          : in  std_logic                     := 'X'; -- clk
+			reset_reset_n    : in  std_logic                     := 'X'; -- reset_n
+			to_7seg_readdata : out std_logic_vector(6 downto 0);         -- readdata
+			to_hex_readdata  : out std_logic_vector(31 downto 0)         -- readdata
 		);
 	end component embedded_system;
 
 	u0 : component embedded_system
 		port map (
-			clk_clk         => CONNECTED_TO_clk_clk,         --    clk.clk
-			reset_reset_n   => CONNECTED_TO_reset_reset_n,   --  reset.reset_n
-			to_hex_readdata => CONNECTED_TO_to_hex_readdata  -- to_hex.readdata
+			clk_clk          => CONNECTED_TO_clk_clk,          --     clk.clk
+			reset_reset_n    => CONNECTED_TO_reset_reset_n,    --   reset.reset_n
+			to_7seg_readdata => CONNECTED_TO_to_7seg_readdata, -- to_7seg.readdata
+			to_hex_readdata  => CONNECTED_TO_to_hex_readdata   --  to_hex.readdata
 		);
 
